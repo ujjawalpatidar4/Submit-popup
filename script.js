8657081f87a18de15692c3abbc8eff713d9eada4
Ujjawal Patidar <118273885+ujjawalpatidar4@users.noreply.github.com>
2024-01-02 11:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup = document.querySelector('#popup')
+
+function openpopup() {
+    popup.classList.add("open-popup")
+}
+function closepopup() {
+    popup.classList.remove("open-popup")
+}
